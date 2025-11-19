bb390b9d9d91b38fa132e81d0e3589f62e516c69
white6md <hoanghai.009.123@gmail.com>
2025-11-20 01:51:28 +0700

    v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from extensions import db, login_manager
 import os
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='.')
     
     # Configuration
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-g4market')
